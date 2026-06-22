01e1476b68876ff651c146f499539f9958f85408
The Tech Shift <officalthetechshift@gmail.com>
2026-06-22 14:24:03 +0530

    git hub commands

---
+++ githubcommands.py
@@ -0,0 +1,71 @@
+# #github commands
+# #github commands are used to interact with GitHub repositories from the command line. Here are some common GitHub commands:
+
+# 1. git clone <repository_url> - This command is used to clone a GitHub repository to your local machine. You need to provide the URL of the repository you want to clone. fOR EXAMPLE:
+#    git clone  https://github.com/officialthetechshift-Futureofnetworks/python_basics.git #here we are cloning the repository python_basics from the GitHub account officialthetechshift-Futureofnetworks to our local machine.
+
+
+# 2. git add <file_name> - This command is used to add a file to the staging area before committing changes.Example
+
+#    git add githubcommands.py #here we are adding the file githubcommands.py to the staging area before committing changes.
+
+# 3. git commit -m "<commit_message>" - This command is used to commit changes to the repository with a descriptive message. example:
+#    git commit -m "Added new GitHub commands" #here we are committing changes to the repository with a descriptive message "Added new GitHub commands".
+
+
+# 4. git push origin <branch_name> - This command is used to push changes from ast import main
+# from logging import config, log from your local repository to the remote repository on GitHub.For example:
+#    git push origin main #here we are pushing changes from our local repository to the remote repository on GitHub on the main branch.
+
+# 5. git pull origin <branch_name> - This command is used to fetch and merge changes from the remote repository to your local repository. for example:
+#    git pull origin main #here we are fetching and merging changes from the remote repository on GitHub to our local repository on the main branch.
+
+# 6. git status - This command is used to check the status of your local repository, including any changes that have been made and files that are staged for commit. For example:
+#    git status #here we are checking the status of our local repository, including any changes that have been made and files that are staged for commit. 
+# where 
+#    - Untracked files: These are files that are not being tracked by Git. They have not been added to the staging area or committed to the repository.
+#    - Changes not staged for commit: These are files that have been modified but have not yet been added to the staging area. They will not be included in the next commit unless they are staged.
+#    - Changes to be committed: These are files that have been added to the staging area and are ready to be committed to the repository. They will be included in the next commit.
+
+# 7. git log - This command is used to view the commit history of the repository. for example:
+#    git log #here we are viewing the commit history of the repository, including the commit hash, author, date, and commit message for each commit.
+
+# 8. git branch - This command is used to list all the branches in the repository or create a new branch. For example:
+#    git branch #here we are listing all the branches in the repository.
+#    git branch <branch_name> #here we are creating a new branch with the name <branch_name>.
+
+# 9. git checkout <branch_name> - This command is used to switch to a different branch. For example:
+#    git checkout <branch_name> #here we are switching to the branch with the name <branch_name>.
+
+# 10. git merge <branch_name> - This command is used to merge changes from one branch into another branch. For example:
+#     git merge <branch_name> #here we are merging changes from the branch with the name <branch_name> into the current branch.
+
+# 11. git remote - This command is used to manage remote repositories. For example:
+#     git remote add <remote_name> <repository_url> #here we are adding a remote repository with the name <remote_name> and the URL <repository_url>.
+
+# 12. git remote remove <remote_name> - This command is used to remove a remote repository. For example:
+#     git remote remove <remote_name> #here we are removing the remote repository with the name
+
+# 13. git fetch - This command is used to fetch changes from the remote repository without merging them into your local repository. For example:
+#     git fetch #here we are fetching changes from the remote repository without merging them into our local repository.
+
+# 14. git reset --hard <commit_hash> - This command is used to reset your local repository to a specific commit, discarding any changes made after that commit. For example:
+#     git reset --hard <commit_hash> #here we are resetting our local repository to the commit with the hash <commit_hash>, discarding any changes made after that commit.
+
+# 15. git stash - This command is used to temporarily save changes that are not ready to be committed. For example:
+#     git stash #here we are temporarily saving changes that are not ready to be committed.   
+
+# 16. git stash pop - This command is used to apply the most recent stashed changes and remove them from the stash list. For example:
+#     git stash pop #here we are applying the most recent stashed changes and removing them from
+
+# 17 . git tag <tag_name> - This command is used to create a tag for a specific commit in the repository. For example:
+#     git tag <tag_name> #here we are creating a tag with the name <tag_name> for a specific commit in the repository.
+
+# 18. git diff - This command is used to view the differences between the working directory and the staging area or between two commits. For example:
+#     git diff #here we are viewing the differences between the working directory and the staging area.
+
+# 19. git config --global user.name "<your_name>" - This command is used to set your Git username globally for all repositories on your machine. For example:
+#     git config --global user.name "The Tech Shift" #here we are setting our Git username globally for all repositories on our machine to "The Tech Shift".
+
+# 20. git config --global user.email "officialthetechshift" - This command is used to set your Git email address globally for all repositories on your machine. For example:
+#     git config --global user.email "officialthetechshift@gmail.com" #here we are setting our Git email address globally for all repositories on our machine to "officialthetechshift@gmail.com"
